99a45b96f9495930e177214ffa01ae8ff0e45c21
Asutosh Ranjan <asutoshranjan.work@gmail.com>
2021-09-21 16:59:35 +0530

    The Mamuli App with Mamuli Level

---
+++ test_project/lib/homepage.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+// ignore_for_file: deprecated_member_use
+
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+
+  int c = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: Colors.grey[900],
+
+      appBar: AppBar(
+        backgroundColor: Color(0xFF121212),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: () {},
+        ),
+        title: Text(
+          'Mamuli App',
+          style: TextStyle(
+            fontSize: 20,
+            letterSpacing: 2,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+
+      body: Column(
+        children: [
+
+          SizedBox(
+            height: 40,
+          ),
+
+          Center(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+              child: Text(
+                'Mamuli Insaan',
+                style: TextStyle(
+                  color: Color(0xFFCF6679),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xFF121212),
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+
+          SizedBox(
+            height: 50,
+          ),
+
+          Center(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 80, vertical: 40),
+              child: Column(
+                children: [
+                  Text(
+                    'Level : ${c}',
+                    style: TextStyle(
+                      color: Color(0xFF03DAC6),
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 3,
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  FlatButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        c -= 1;
+                      });
+                    },
+                    icon: Icon(
+                      Icons.delete,
+                      color: Color(0xFFBB86FE),
+                    ),
+                    label: Text(
+                      'Downgrade Level',
+                      style: TextStyle(
+                        color: Color(0xFFBB86FE),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                color: Colors.indigo[900],
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            c += 1;
+          });
+        },
+        backgroundColor: Colors.indigo,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
